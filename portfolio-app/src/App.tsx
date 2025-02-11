6488bc2e40e9f79fe3d3400ac760279bf1208dd4
Nathan Fassler <nathanjellybeans99@gmail.com>
2025-02-11 15:50:48 -0800

    #19 added App.tsx file and placeholder about section

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
 
   return (
     <div className="bg-teal-50">
        <Navbar />
+        <About />
     </div>
    
   )
---
+++ portfolio-app/src/components/About.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <section id="about" className="py-20 text-center max-w-4xl mx-auto">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Hi, my name is Nathan Fassler.
+      </h1>
+      <p className="text-lg text-gray-700 leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra
+        mauris porta, convallis nibh eu, tempor neque. Vestibulum luctus
+        rhoncus justo a tempus. Quisque elementum porttitor mauris ac
+        vulputate. Donec sit amet justo ut nibh blandit vehicula. Ut nec ex.
+      </p>
+    </section>
+  );
+};
+
+export default About;
