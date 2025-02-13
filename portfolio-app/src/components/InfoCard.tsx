e090a78693f7cb9d4e15e63e24fe209f09998b7d
Nathan Fassler <nathanjellybeans99@gmail.com>
2025-02-12 20:08:11 -0800

    #13 changed alt text for image from InfoCard title to imageUrl

--- portfolio-app/src/components/InfoCard.tsx
+++ portfolio-app/src/components/InfoCard.tsx
@@ -13,9 +13,9 @@ const InfoCard: React.FC<InfoCardProps> = ({ title, description, imageUrl, linkU
       href={linkUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="block bg-lightblue p-6 rounded-lg shadow-md hover:shadow-2xl transition-shadow duration-200"
+      className="block bg-lightblue p-6 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-200"
     >
-      <img src={imageUrl} alt={title} className="w-full h-80 object-cover rounded-md mb-4" />
+      <img src={imageUrl} alt={imageUrl} className="w-full h-80 object-cover rounded-md mb-4" />
       <h3 className="text-lg font-bold font-['Anonymous_Pro']">{title}</h3>
       <p className="text-sm">{description}</p>
     </a>
